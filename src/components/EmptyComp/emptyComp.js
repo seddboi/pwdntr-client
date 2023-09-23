483f4b0624092028b30ee3ed7f087b3bd5ca2cd6
Gian Zamora <gz2016@att.net>
2023-09-22 21:18:45 -0700

    adjusted sizing of empty div text

--- src/components/EmptyComp/emptyComp.js
+++ src/components/EmptyComp/emptyComp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography } from '@mui/material';
 export function EmptyComp() {
 	return (
-		<Typography variant="h2" sx={{ color: '#ffffff' }}>
+		<Typography variant="h4" sx={{ color: '#ffffff' }}>
 			No data to show.
 		</Typography>
 	);
