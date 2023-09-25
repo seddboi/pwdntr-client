91db88e577f60ab3adcf913a7b2094541d5f041d
Gian Zamora <gz2016@att.net>
2023-09-24 18:57:17 -0700

    adjusted logout errors and adjusted localSTorage to sessionStorage

--- src/components/SideMenu/sidemenu.js
+++ src/components/SideMenu/sidemenu.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { FolderShared, Home, Logout } from '@mui/icons-material';
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { APP_URL, theme } from '../../App';
 
-export function SideMenu({open, onClose, isHome}) {
+export function SideMenu({ open, onClose, isHome }) {
 	const location = useLocation();
 
 	const [isLoggedIn, setIsLoggedIn] = useState(location.state.status);
@@ -17,43 +17,53 @@ export function SideMenu({open, onClose, isHome}) {
 	const logout = async () => {
 		await Axios.post(APP_URL + '/logout', {
 			userID: location.state.user.id,
-			token: localStorage.getItem('aT'),
+			token: sessionStorage.getItem('aT'),
 		}).then((res) => {
 			setIsLoggedIn(res.data.auth);
-			JSON.parse(localStorage.removeItem('aT'));
-		})
+			sessionStorage.removeItem('aT');
+		});
 	};
 
 	useEffect(() => {
 		if (!isLoggedIn) {
-			navigate('/login', {state: {status: isLoggedIn }, replace: true})		
-		};
+			navigate('/login', { state: { status: isLoggedIn }, replace: true });
+		}
 		// eslint-disable-next-line
-	}, [isLoggedIn])
+	}, [isLoggedIn]);
 
 	return (
 		<ThemeProvider theme={theme}>
-			<Drawer open={open} onClose={onClose} PaperProps={{sx:{color: 'white', backgroundColor: '#17202A'}}}>
+			<Drawer open={open} onClose={onClose} PaperProps={{ sx: { color: 'white', backgroundColor: '#17202A' } }}>
 				<List>
 					<ListItem disablePadding>
-						<ListItemButton onClick={() => {navigate(pageSelector, {state: {status: isLoggedIn, user: {id: location.state.user.id, username: location.state.user.username} }})}}>
-							<ListItemIcon sx={{color: 'white'}}>
-								{isHome ? <FolderShared fontSize='large'/> : <Home fontSize='large'/>}
+						<ListItemButton
+							onClick={() => {
+								navigate(pageSelector, {
+									state: { status: isLoggedIn, user: { id: location.state.user.id, username: location.state.user.username } },
+								});
+							}}
+						>
+							<ListItemIcon sx={{ color: 'white' }}>
+								{isHome ? <FolderShared fontSize="large" /> : <Home fontSize="large" />}
 							</ListItemIcon>
-							<ListItemText color='primary' primary={isHome ? 'Saved Passwords' : 'Home'}></ListItemText>
+							<ListItemText color="primary" primary={isHome ? 'Saved Passwords' : 'Home'}></ListItemText>
 						</ListItemButton>
 					</ListItem>
 
 					<ListItem disablePadding>
-						<ListItemButton onClick={() => {logout()}}>
-							<ListItemIcon sx={{color: 'white'}}>
-								<Logout fontSize='large'/>
+						<ListItemButton
+							onClick={() => {
+								logout();
+							}}
+						>
+							<ListItemIcon sx={{ color: 'white' }}>
+								<Logout fontSize="large" />
 							</ListItemIcon>
-							<ListItemText primary='Logout'></ListItemText>
+							<ListItemText primary="Logout"></ListItemText>
 						</ListItemButton>
 					</ListItem>
 				</List>
 			</Drawer>
 		</ThemeProvider>
-	)
-};
+	);
+}
--- src/pages/Signup/signup.js
+++ src/pages/Signup/signup.js
@@ -1,16 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { Box, AppBar, Toolbar, TextField, Typography, Button, IconButton, InputAdornment } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
-import { APP_URL, theme } from '../../App'
+import { APP_URL, theme } from '../../App';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 export function Signup() {
-
 	const [regUsername, setRegUsername] = useState('');
 	const [regPassword, setRegPassword] = useState('');
-	const [showPassword ,setShowPassword] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
 	const [regEmail, setRegEmail] = useState('');
 	const [loginStatus, setLoginStatus] = useState(false);
 	const [userInfo, setUserInfo] = useState({});
@@ -25,55 +24,109 @@ export function Signup() {
 
 	const signup = async () => {
 		await Axios.post(APP_URL + '/signup', {
-			username: regUsername, 
-			password: regPassword, 
+			username: regUsername,
+			password: regPassword,
 			email: regEmail,
-	}).then((res) => {
-		if (res.data.auth) {
-			localStorage.setItem('aT', res.data.token);
-			setLoginStatus(res.data.auth);
-			setUserInfo(res.data.result);
-		} else {
-			setSignupErrorMessage(res.data.message);
-			setSignupError(true);
-		}
-	})};
+		}).then((res) => {
+			if (res.data.auth) {
+				sessionStorage.setItem('aT', res.data.token);
+				setLoginStatus(res.data.auth);
+				setUserInfo(res.data.result);
+			} else {
+				setSignupErrorMessage(res.data.message);
+				setSignupError(true);
+			}
+		});
+	};
 
 	useEffect(() => {
 		if (loginStatus) {
-			navigate('/home', {state: {status: loginStatus, user: userInfo}, replace: true} );
-		};
+			navigate('/home', { state: { status: loginStatus, user: userInfo }, replace: true });
+		}
 		// eslint-disable-next-line
 	}, [loginStatus]);
 
 	return (
 		<ThemeProvider theme={theme}>
-			<Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
-				<Box component='nav' sx={{ flexGrow: 1, margin: 0, width: '100%' }}>
-					<AppBar position='static'>
+			<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+				<Box component="nav" sx={{ flexGrow: 1, margin: 0, width: '100%' }}>
+					<AppBar position="static">
 						<Toolbar>
-							<Typography variant='h4'  sx={{display: {xs: 'none', sm:'block'}, flexGrow:1, textAlign:'start', letterSpacing: {xs: 'none', sm: '0.4em',md:'0.6em'}}}>Passwordinator</Typography>
-							<Button color='inherit' onClick={() => {navigate('/')}}>Generator</Button>
-							<Button color='inherit' onClick={() => {navigate('/login')}}>Login</Button>
+							<Typography
+								variant="h4"
+								sx={{
+									display: { xs: 'none', sm: 'block' },
+									flexGrow: 1,
+									textAlign: 'start',
+									letterSpacing: { xs: 'none', sm: '0.4em', md: '0.6em' },
+								}}
+							>
+								Passwordinator
+							</Typography>
+							<Button
+								color="inherit"
+								onClick={() => {
+									navigate('/');
+								}}
+							>
+								Generator
+							</Button>
+							<Button
+								color="inherit"
+								onClick={() => {
+									navigate('/login');
+								}}
+							>
+								Login
+							</Button>
 						</Toolbar>
 					</AppBar>
 				</Box>
-				<Box component='form' sx={{display: 'flex', flexDirection: 'column', width: '90%',maxWidth: '500px', margin: 4}}>
-					<Typography variant='h2' color='#ffffff' sx={{mb: '70px'}}>Signup Below!</Typography>
-					<TextField label="Username" variant="outlined" onChange={(e) => {setRegUsername(e.target.value)}} sx={{mt: 2, mb: 2}}/>
-					<TextField label="Email" variant="outlined" error={signupError} onChange={(e) => {setRegEmail(e.target.value)}} sx={{mt: 2, mb: 2}}/>		
-					<TextField label="Password" type={showPassword ? 'text' : 'password'} variant="outlined" onChange={(e) => {setRegPassword(e.target.value)}} InputProps={{
-						endAdornment: 
-							<InputAdornment position='end'>
-								<IconButton onClick={handleVisibility}>
-									{showPassword ? <VisibilityOff /> : <Visibility />}
-								</IconButton>
-							</InputAdornment>
-					}} sx={{mt: 2, mb: 2}}/>	
-					<Typography id='submitMessage' variant='h6' sx={{color: 'red', mb: 8}}>{signupErrorMessage}</Typography>
+				<Box component="form" sx={{ display: 'flex', flexDirection: 'column', width: '90%', maxWidth: '500px', margin: 4 }}>
+					<Typography variant="h2" color="#ffffff" sx={{ mb: '70px' }}>
+						Signup Below!
+					</Typography>
+					<TextField
+						label="Username"
+						variant="outlined"
+						onChange={(e) => {
+							setRegUsername(e.target.value);
+						}}
+						sx={{ mt: 2, mb: 2 }}
+					/>
+					<TextField
+						label="Email"
+						variant="outlined"
+						error={signupError}
+						onChange={(e) => {
+							setRegEmail(e.target.value);
+						}}
+						sx={{ mt: 2, mb: 2 }}
+					/>
+					<TextField
+						label="Password"
+						type={showPassword ? 'text' : 'password'}
+						variant="outlined"
+						onChange={(e) => {
+							setRegPassword(e.target.value);
+						}}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton onClick={handleVisibility}>{showPassword ? <VisibilityOff /> : <Visibility />}</IconButton>
+								</InputAdornment>
+							),
+						}}
+						sx={{ mt: 2, mb: 2 }}
+					/>
+					<Typography id="submitMessage" variant="h6" sx={{ color: 'red', mb: 8 }}>
+						{signupErrorMessage}
+					</Typography>
 				</Box>
-				<Button color='primary' variant='contained' size='large' onClick={signup}>Signup</Button>
+				<Button color="primary" variant="contained" size="large" onClick={signup}>
+					Signup
+				</Button>
 			</Box>
 		</ThemeProvider>
-	)
-};
+	);
+}
