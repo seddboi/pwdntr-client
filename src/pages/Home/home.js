d6b00687cfbc546d45bc1b4415d5e5b79953e5f5
Gian Zamora <gz2016@att.net>
2023-11-27 21:24:42 -0800

    updated home page after sign in to match generic password generator page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
 import { Layout } from '../layout/layout.js';
-import { GeneratorComp } from '../../components/GeneratorComp/generatorcomp';
+import { Form } from '../../components/Form/form.js';
 
 import './home.css';
 
 export function Home() {
 	return (
 		<Layout>
-			<Box component="main">
-				<Typography className="subhead-title" variant="h5" sx={{ mt: 3, fontSize: '20px', color: '#eeeeee' }}>
-					Password Generator
-				</Typography>
-				<GeneratorComp />
+			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+				<Box
+					sx={{
+						maxWidth: '800px',
+						width: '100%',
+						mt: { xs: 3, sm: 5 },
+						mx: 2,
+						p: { xs: 2, sm: 5 },
+						borderRadius: '8px',
+						boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+						bgcolor: 'container.main',
+					}}
+				>
+					<Box className="top-section" component="main" sx={{ my: 5, mx: 2 }}>
+						<Typography
+							className="subhead-title prevent-select"
+							variant="h4"
+							sx={{ fontSize: '40px', color: 'container.contrastText' }}
+						>
+							PASSWORD RANDOMIZER
+						</Typography>
+					</Box>
+					<Form />
+				</Box>
 			</Box>
 		</Layout>
 	);
